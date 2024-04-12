9037ad65104e9d34786cc61bbacff86042cee40c
Chloe <enderpuff@proton.me>
2024-04-12 07:47:35 -0300

    Use flexbox goddamn it

--- res/js/translate.js
+++
@@ -1,8 +0,0 @@
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-var translator = $('body').translate({lang: "en", t: dict.translations}); //English by default
-
-/*sleep(5000).then(()=>{
-    translator.lang("pt"); //Change to Portuguese
-});*/
